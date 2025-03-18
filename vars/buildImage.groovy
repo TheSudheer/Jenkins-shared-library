9f57bffe39975dfc28fae957b0da822dea627e03
sudheer <sudheersunny129@gmail.com>
2025-03-18 14:40:06 +0530

    Added buildImage: logic for building a docker Image

---
+++ vars/buildImage.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy // shebang line
+
+def call{
+    echo 'Building the docker image'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+        sh "docker build -t kalki2878/java-gradle-app:latest ."
+        sh "echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin"
+        sh "docker push kalki2878/java-gradle-app:latest"
+    }
+}
+
+return this
