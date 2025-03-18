19c2f6feaae6bff355cab1be95ba77d3d74a240b
sudheer <sudheersunny129@gmail.com>
2025-03-18 16:38:55 +0530

    Parameterized the buildImage

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,10 @@
-#!/usr/bin/env groovy // shebang line
+#!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo 'Building the docker image'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-        sh "docker build -t kalki2878/java-gradle-app:latest ."
+        sh "docker build -t ${imageName} ."
         sh "echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin"
-        sh "docker push kalki2878/java-gradle-app:latest"
+        sh "docker push ${imageName}"
     }
 }
-
-return this
