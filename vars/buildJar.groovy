87aaa97a6db116cc69df4136c8cfaa7f7697123d
sudheer <sudheersunny129@gmail.com>
2025-03-28 17:56:20 +0530

    Add runtime version checks for Java & Gradle in build script
    - Print out Java and Gradle versions for verification.
    - Invoke the Gradle build with a 180-second timeout.

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,15 @@
-#!/usr/bin/env groovy // shebang line
+#!/usr/bin/env groovy
 
 def call() {
-    echo "building the application...."
-    sh " timeout --kill-after=180 180 ./gradlew clean build"
+    echo "Checking Java and Gradle versions before building the application..."
+    sh '''
+        echo "---- Java Version ----"
+        java -version
+
+        echo "---- Gradle Version ----"
+        gradle --version
+
+        echo "Building the application with a timeout of 180 seconds..."
+        timeout --kill-after=180 180 gradle clean build
+    '''
 }
