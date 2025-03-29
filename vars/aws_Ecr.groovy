b0aac85b6f8847f6c5704827b462f1687dd0099f
sudheer <sudheersunny129@gmail.com>
2025-03-29 16:50:00 +0530

    added aws ecr uri

--- vars/aws_Ecr.groovy
+++ vars/aws_Ecr.groovy
@@ -4,7 +4,7 @@ def call(String AWS_ECR_REPO, String imageName) {
     echo 'Building the docker image and pushing it to AWS ECR...'
     withCredentials([usernamePassword(credentialsId: 'ecr-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
         sh "docker build -t ${AWS_ECR_REPO}:${imageName} ."
-        sh "echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin"
+        sh "echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin ${AWS_ECR_SERVER}"
         sh "docker push ${AWS_ECR_REPO}:${imageName}"
     }
 }
